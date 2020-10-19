f2fe56654aee354f3dd74f94f963688e4dfe2a48
laller2000 <laller2000@outlook.com>
2020-10-19 18:35:35 +0200

    Add project files.

---
+++ WindowsFormsOrszagok/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsFormsOrszagok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView_Orszagok = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Orszagok)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_Orszagok
+            // 
+            this.dataGridView_Orszagok.AllowUserToAddRows = false;
+            this.dataGridView_Orszagok.AllowUserToDeleteRows = false;
+            this.dataGridView_Orszagok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Orszagok.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView_Orszagok.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView_Orszagok.Name = "dataGridView_Orszagok";
+            this.dataGridView_Orszagok.ReadOnly = true;
+            this.dataGridView_Orszagok.RowHeadersWidth = 51;
+            this.dataGridView_Orszagok.RowTemplate.Height = 24;
+            this.dataGridView_Orszagok.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView_Orszagok.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_Orszagok);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Orszagok adatbazis";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Orszagok)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_Orszagok;
+    }
+}
+
---
+++ WindowsFormsOrszagok/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsOrszagok
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection = null;
+        MySqlCommand sql = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView_Orszagok.ColumnCount = 3;
+            dataGridView_Orszagok.AutoSizeColumnsMode= DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView_Orszagok.Columns[0].Name = "orszag_neve";
+            dataGridView_Orszagok.Columns[0].HeaderText = "Ország neve";
+            dataGridView_Orszagok.Columns[1].Name = "fovaros";
+            dataGridView_Orszagok.Columns[1].HeaderText = "Főváros";
+            dataGridView_Orszagok.Columns[2].Name = "terulet";
+            dataGridView_Orszagok.Columns[2].HeaderText = "Terület";
+
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Server = "localhost";
+            sb.UserID = "root";
+            sb.Password = "";
+            sb.Database = "orszagok";
+            connection = new MySqlConnection(sb.ToString());
+            try
+            {
+                connection.Open();
+                sql = connection.CreateCommand();
+            }
+            catch (MySqlException myex)
+            {
+                MessageBox.Show(myex.Message);
+                Environment.Exit(0);
+                throw;
+            }
+            sql.CommandText = "SELECT `orszag`,`fovaros`,`terulet` FROM `orszagok` WHERE 1";
+            using (MySqlDataReader dr=sql.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    //új adatsor
+                    int uj_sor_indexe = dataGridView_Orszagok.Rows.Add();
+                    //már formázott sor
+                    DataGridViewRow uj_sor = dataGridView_Orszagok.Rows
+                        [uj_sor_indexe];
+                    uj_sor.Cells["orszag_neve"].Value=dr.GetString("orszag");
+                    uj_sor.Cells["fovaros"].Value = dr.GetString("fovaros");
+                    uj_sor.Cells["terulet"].Value = dr.GetDouble("terulet");
+                }
+            }
+
+        }
+    }
+}
